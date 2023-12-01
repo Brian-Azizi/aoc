658513964ecd2def7c9e5f9831ca1d85754fa267
Brian Azizi <brian@azizi.dev>
2023-12-01 22:09:06 +0000

    One

---
+++ 1/one.ts
@@ -0,0 +1,30 @@
+import { INPUT } from "./INPUT";
+
+const DIGITS = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+type Digit = keyof typeof DIGITS;
+
+const main = () =>
+  INPUT.trim()
+    .split("\n")
+    .map((line) =>
+      line.replaceAll(
+        /(one|two|three|four|five|six|seven|eight|nine)/g,
+        (match) => DIGITS[match as Digit],
+      ),
+    )
+    .map((line) => line.match(/\d/g))
+    .map((matches) => [matches?.[0], matches?.slice(-1)[0]].join(""))
+    .map((str) => parseInt(str, 10))
+    .reduce((acc, num) => acc + num, 0);
+
+console.log(main());
